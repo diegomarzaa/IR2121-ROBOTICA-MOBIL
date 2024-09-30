d79c4ad9c88703ac94956dce131f482d07f4a7f9
diegomarzaa <diegomarzafuertes@gmail.com>
2024-09-30 20:17:52 +0200

    Worlds Nova Carpeta

--- Teleoperation/Scripts/launch_tb3_webots.bash
+++ Teleoperation/Scripts/launch_tb3_webots.bash
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Source ROS2 Humble setup
 source /opt/ros/humble/setup.bash
-
-# Restrict ROS communication to localhost
 export ROS_LOCALHOST_ONLY=1
-
-# Set the Webots home directory
 export WEBOTS_HOME=$HOME/webots-R2023b
-
-# Launch the Webots TurtleBot3 simulation
 ros2 launch webots_ros2_turtlebot robot_launch.py
