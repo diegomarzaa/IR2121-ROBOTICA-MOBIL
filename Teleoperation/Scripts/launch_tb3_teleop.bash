30cb1f06a24a561acfceb4e08f9c02c7123d2260
diegomarzaa <al426641@uji.es>
2024-10-02 16:56:13 +0200

    robot fisic proves

--- Teleoperation/Scripts/launch_tb3_teleop.bash
+++ Teleoperation/Scripts/launch_tb3_teleop.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_LOCALHOST_ONLY=1
+export ROS_DOMAIN_ID=8
+export ROS_LOCALHOST_ONLY=0
 export TURTLEBOT3_MODEL=burger
 ros2 run turtlebot3_teleop teleop_keyboard
--- Worlds/scripts/visualize_robot.bash
+++ Worlds/scripts/visualize_robot.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_LOCALHOST_ONLY=1
+export ROS_DOMAIN_ID=8
+export ROS_LOCALHOST_ONLY=0
+export TURTLEBOT3_MODEL=burger
 rviz2 -d config_robot.rviz
-
