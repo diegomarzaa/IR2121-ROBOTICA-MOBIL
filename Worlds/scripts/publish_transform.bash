741b6f1ab89d9e6234110245f6085ac0de49984e
diegomarzaa <diegomarzafuertes@gmail.com>
2024-10-09 18:00:26 +0200

    Editor

--- Worlds/scripts/publish_transform.bash
+++ Worlds/scripts/publish_transform.bash
@@ -3,5 +3,5 @@ source /opt/ros/humble/setup.bash
 export ROS_DOMAIN_ID=30
 export ROS_LOCALHOST_ONLY=0
 export TURTLEBOT3_MODEL=burger
-ros2 run tf2_ros static_transform_publisher --x -5.5 --y 16.1 --z 0 \
+ros2 run tf2_ros static_transform_publisher --x -5.3 --y 16.4 --z 0 \
   --qx 0 --qy 0 --qz 0 --qw 1 --frame-id map --child-frame-id odom
