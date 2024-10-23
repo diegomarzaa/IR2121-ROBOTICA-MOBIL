eef406d87255d212b944d75b8b52c37a5444ba70
diegomarzaa <diegomarzafuertes@gmail.com>
2024-10-23 10:47:41 +0200

    Updates

--- Teleoperation/Scripts/launch_ros2_bag_record.bash
+++ Teleoperation/Scripts/launch_ros2_bag_record.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_DOMAIN_ID=30
-export ROS_LOCALHOST_ONLY=0
+export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 bag record /clock /map /odom /tf /tf_static /scan
+ros2 bag record /clock /map /odom /scan /tf /tf_static /amcl_pose /particle_cloud /robot_description
--- Teleoperation/Scripts/launch_tb3_teleop.bash
+++ Teleoperation/Scripts/launch_tb3_teleop.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
-export ROS_DOMAIN_ID=30
-export ROS_LOCALHOST_ONLY=0
+export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
 ros2 run turtlebot3_teleop teleop_keyboard
